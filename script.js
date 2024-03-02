a39858041840fe6b1fd1cff4fdd43ad792debcca
gracepal <gracepal810@gmail.com>
2024-03-02 06:32:11 -0800

    move around buttons

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('hello from scripts', data)
+console.log('hello from scripts')
+const todayBtnEl = document.querySelector('button.next + button.today')
+
+todayBtnEl.addEventListener('click', function (e) {
+  console.log('clicked on button to go to today')
+})
--- setup.js
+++ setup.js
@@ -1,25 +1,54 @@
-const data = { message: 'hello from setup file' }
+// Elements
+const calendarEl = document.querySelector('.calendar')
 
 /**
  *
  */
-function setupNavbar() {
-  // setup current date
-  const todayDisplayEl = document.querySelector('.header .today')
-  let today = new Date()
-  todayDisplayEl.textContent = today
-}
+function setupNavbar() {}
 
 /**
  *
  */
 function setupCalendar() {
   //
+  const dimensions = Utils.getGridDimensions()
+  const numWeekRows = 6
+  let curDate = -5 // -startDayOfWeek+1
+  let todayDate = new Date().getDate()
+  calendarEl.style.gridTemplateRows = `repeat(${numWeekRows}, minmax(180px, 1fr));`
+
+  for (let i = 0; i < numWeekRows; i++) {
+    const weekEl = document.createElement('div')
+    weekEl.className = `week row-${i + 1}`
+    for (let j = 0; j < 7; j++) {
+      const dayEl = document.createElement('div')
+      dayEl.className = `day col-${i + 1}`
+      const dateEl = document.createElement('div')
+      dateEl.className = 'date'
+      if (curDate >= 1) {
+        dateEl.textContent = curDate
+        if (curDate == todayDate) {
+          dateEl.className += ' active'
+        }
+      }
+      dayEl.appendChild(dateEl)
+      weekEl.appendChild(dayEl)
+      curDate += 1
+    }
+    calendarEl.appendChild(weekEl)
+  }
 }
 
 /**
  *
  */
-function setupFooter() {}
+function setupFooter() {
+  // setup current date
+  const todayDisplayEl = document.querySelector('.footer .today')
+  let today = new Date()
+  todayDisplayEl.textContent = today
+}
 
-setupNavbar()
+// Setup
+setupCalendar()
+setupFooter()
